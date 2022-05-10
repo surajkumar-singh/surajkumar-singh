fa076c51757c7515640c89fea6ab99adfc649d8d
Surajkumar Singh <surajkumar.singh@searce.com>
2022-05-10 17:39:12 +0530

    done

--- gcp-deployment/gcp-deployment/env/prod/global/iam/terraform.tfvars
+++ gcp-deployment/gcp-deployment/env/prod/global/iam/terraform.tfvars
@@ -1,4 +1,4 @@
-project_id = "simple-sample-project-123-6a20"
+project_id = "seed-348602"
 region     = "us-east4"
 service_account_mgmt_vm = {
   service_account = "as-mgmt-vm-sa"
--- gcp-deployment/gcp-deployment/env/prod/resource-hierarchy/simple_project/backend.tf
+++ gcp-deployment/gcp-deployment/env/prod/resource-hierarchy/simple_project/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
     bucket = "sf-terraform-state-us-est4-01-gcs"
-    prefix = "gcp/env/pilot/global/projects/prod"
+    #prefix = "gcp/env/pilot/global/projects/prod"
   }
 }
